ed34ec274ed1d6c5535f33d4933a19e5170c0dd6
Roman-Ojha <razzroman98@gmail.com>
2023-03-06 21:37:43 +0545

    build: init project

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*
+    *) Steps:
+        -> first we have to download mysqli connector
+        -> https://dev.mysql.com/downloads/connector/cpp/
+        -> https://github.com/agusk/crud-mysql-cpp/blob/master/src/demo-connection.cpp
+*/
+int main()
+{
+    std::cout << "Hello World!\n";
+}
